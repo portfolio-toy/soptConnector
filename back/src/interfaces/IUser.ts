da4f6f2070f059e2519eded4fd4663b088eaa1d4
Kayoung <rkdkdudjd@naver.com>
2021-04-10 17:33:13 +0900

    Feat: 회원가입 api

---
+++ back/src/interfaces/IUser.ts
@@ -0,0 +1,14 @@
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+  date: Date;
+}
+
+export interface IUserInputDTO {
+  name: string;
+  email: string;
+  password: string;
+}
