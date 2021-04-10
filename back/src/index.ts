5ca217809432618a5cd4a3b96c1de2dde2c26126
Kim HyunJi <khyunjiee@gmail.com>
2021-04-10 17:09:54 +0900

    feat: 회원가입 API

--- back/src/index.ts
+++ back/src/index.ts
@@ -5,7 +5,10 @@ import connectDB from "./Logger/db";
 // Connect Database
 connectDB();
 
+app.use(express.json());
+
 // Define Routes
+app.use("/api/users", require("./api/users"));
 
 // error handler
 app.use(function (err, req, res, next) {
