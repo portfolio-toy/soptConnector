90c3af7b2b56735a9de96f34cc376b6037640b2f
최영재 <brandon94_@naver.com>
2021-04-10 17:09:42 +0900

    feat: 회원가입 API

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,11 +1,20 @@
+// express는 프레임 워크인데 서버 구축을 도와주는 프레임 워크이다. 
 import express from "express";
+
 const app = express();
+
+// 이건 데이터 베이스에 붙여주는 것.
+// 우리는 몽고 db를 사용할텐데 관련 코드는 3주차 이후에 알게 될 것.
 import connectDB from "./Logger/db";
 
 // Connect Database
 connectDB();
 
+app.use(express.json());
+
 // Define Routes
+app.use("/api/users", require("./api/users"));
+
 
 // error handler
 app.use(function (err, req, res, next) {
