c79abb83869f06218722d9d71ce6ce28cf3ad146
Kim HyunJi <khyunjiee@gmail.com>
2021-04-10 15:32:59 +0900

    chore: 코드 설명 주석 추가

--- back/src/config/index.ts
+++ back/src/config/index.ts
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const envFound = dotenv.config();
+const envFound = dotenv.config();                                // dotenv의 파일을 긁어오는 것
 if (envFound.error) {
   // This error should crash whole process
 
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
-
+// 에러가 없는 클린한 구역 -> dotenv에 넣어놨던 정보를 가져올 수 있음
 export default {
   /**
    * Your favorite port
