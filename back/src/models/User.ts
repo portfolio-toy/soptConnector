4be5cca770b95f3a025748ed8bb0589b350bb9ac
LEEHUIWON <12171941@inha.edu>
2021-04-10 17:10:02 +0900

    feat: 회원가입 API

---
+++ back/src/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { type } from "node:os";
+import {IUser} from "../interfaces/IUser"
+
+const UserSchema = new
+mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default
+mongoose.model<IUser &
+mongoose.Document>("User",UserSchema);
