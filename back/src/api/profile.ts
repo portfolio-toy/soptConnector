cebfd6fe7b2c5b382aae94dd7f766b568738b2a1
kkoon9 <rndrnjs2003@gmail.com>
2021-04-23 20:50:29 +0900

    feat: 학력 추가

--- back/src/api/profile.ts
+++ back/src/api/profile.ts
@@ -224,4 +224,54 @@ router.put(
     }
   }
 );
+
+/**
+ *  @route PUT api/profile/education
+ *  @desc Add profile education
+ *  @access Private
+ */
+router.put(
+  "/education",
+  auth,
+  [
+    check("school", "School is required").not().isEmpty(),
+    check("degree", "Degree is required").not().isEmpty(),
+    check("fieldofstudy", "Field of study is required").not().isEmpty(),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 module.exports = router;
