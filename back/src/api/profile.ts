504de061b610e01ac83c603dff542541e3747dc3
kkoon9 <rndrnjs2003@gmail.com>
2021-04-23 20:49:53 +0900

    feat: 경력 추가

--- back/src/api/profile.ts
+++ back/src/api/profile.ts
@@ -174,4 +174,54 @@ router.post(
     }
   }
 );
+
+/**
+ *  @route PUT api/profile/experience
+ *  @desc Add profile experience
+ *  @access Private
+ */
+router.put(
+  "/experience",
+  auth,
+  [
+    check("title", "Title is required").not().isEmpty(),
+    check("company", "Company is required").not().isEmpty(),
+    check("from", "From data is required").not().isEmpty(),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.body.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 module.exports = router;
