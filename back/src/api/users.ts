461ab6ebade6d66f779922733fe90a096638a510
kkoon9 <rndrnjs2003@gmail.com>
2021-04-23 21:11:44 +0900

    3차 자율과제 브랜치

--- back/src/api/users.ts
+++ back/src/api/users.ts
@@ -24,64 +24,7 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { name, email, password } = req.body;
-
-    try {
-      // See if  user exists
-      let user = await User.findOne({ email });
-
-      if (user) {
-        res.status(400).json({
-          errors: [{ msg: "User already exists" }],
-        });
-      }
-
-      // Get users gravatar
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pq",
-        d: "mm",
-      });
-
-      user = new User({
-        name,
-        email,
-        avatar,
-        password,
-      });
-
-      // Encrpyt password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      // Return jsonwebtoken
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      jwt.sign(
-        payload,
-        config.jwtSecret,
-        { expiresIn: 36000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
+  async (req: Request, res: Response) => {}
 );
 
 module.exports = router;
