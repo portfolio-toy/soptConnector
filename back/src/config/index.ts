d10711a519af2d8ea63f220c0f95b08f8a1648e6
wonseok <wurijang@gmail.com>
2021-04-25 01:39:59 +0900

    [Seminar] 3차 세미나

--- back/src/config/index.ts
+++ back/src/config/index.ts
@@ -28,4 +28,10 @@ export default {
    */
   jwtSecret: process.env.JWT_SECRET,
   jwtAlgorithm: process.env.JWT_ALGO,
+  
+  /**
+  * Your secret sauce
+  */
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubSecret: process.env.GITHUB_SECRET,
 };
