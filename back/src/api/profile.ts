dc61504bf3efb366c0302c31111cac35bc9398cd
kkoon9 <rndrnjs2003@gmail.com>
2021-04-24 16:43:15 +0900

    feat: 저장:

--- back/src/api/profile.ts
+++ back/src/api/profile.ts
@@ -1,7 +1,10 @@
 import express from "express";
-
+import request from "request";
+import config from "../config";
 import Profile from "../models/Profile";
 const router = express.Router();
+
+import auth from "../middlewares/auth";
 
 /**
  *  @route GET api/profile
@@ -40,4 +43,52 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+/**
+ *  @route GET api/profile/github/:username
+ *  @desc Get user repos from github
+ *  @access Public
+ */
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.githubClientId}&client_secret=${config.githubSecret}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode != 200) {
+        res.status(404).json({ msg: "No github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ *  @route GET api/profile/me
+ *  @desc Get current users profile
+ *  @access Private
+ */
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.body.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
