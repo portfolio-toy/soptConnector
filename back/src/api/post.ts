32f03c3fbd1503dd72d112a24ea2a7a1f8eee45a
kkoon9 <rndrnjs2003@gmail.com>
2021-05-02 13:16:27 +0900

    feat: 해당 게시글 조회 API
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- back/src/api/post.ts
+++ back/src/api/post.ts
@@ -55,4 +55,25 @@ router.get("/", auth, async (req: Request, res: Response) => {
   }
 });
 
+/**
+ *  @route GET api/posts/:id
+ *  @desc Get post by ID
+ *  @access Private
+ */
+router.get("/:id", auth, async (req: Request, res: Response) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
