f2e90eae7c1e348a22829590611f5962f8442389
kkoon9 <rndrnjs2003@gmail.com>
2021-05-02 13:25:52 +0900

    feat: 게시글 삭제 API
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- back/src/api/post.ts
+++ back/src/api/post.ts
@@ -76,4 +76,30 @@ router.get("/:id", auth, async (req: Request, res: Response) => {
     res.status(500).send("Server Error");
   }
 });
+
+/**
+ *  @route DELETE api/posts/:id
+ *  @desc Delete a post
+ *  @access Private
+ */
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (post.user.toString() !== req.body.user.id) {
+      return res.status(401).json({ msg: "User not Authorized" });
+    }
+    await post.remove();
+
+    res.json({ msg: "Post Removed" });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
