9bc2d3ce861a6c031a74e5c300edfe2bea44f02d
kkoon9 <rndrnjs2003@gmail.com>
2021-05-02 14:45:42 +0900

    feat: 좋아요 취소 API
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- back/src/api/post.ts
+++ back/src/api/post.ts
@@ -131,4 +131,37 @@ router.put("/like/:id", auth, async (req: Request, res: Response) => {
     res.status(500).send("Server Error");
   }
 });
+
+/**
+ *  @route PUT api/posts/unlike/:id
+ *  @desc Unlike a post
+ *  @access Private
+ */
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (
+      post.likes.filter((like) => like.user.toString() === req.body.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+    const removeIndex = post.likes
+      .map((like) => like.user)
+      .indexOf(req.body.user.id);
+
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
