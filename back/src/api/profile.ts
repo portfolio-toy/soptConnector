71a24497e67fec1cfca4565547d990435551e23e
Kim HyunJi <khyunjiee@gmail.com>
2021-05-08 06:51:43 +0900

    feat: github 프로필 조회 라우터 구현

--- back/src/api/profile.ts
+++ back/src/api/profile.ts
@@ -55,7 +55,28 @@ router.get("/user/:user_id", async (req: Request, res: Response) => {
  *  @desc Get user repos from github
  *  @access Public
  */
-router.get("/github/:username", (req: Request, res: Response) => {});
+router.get("/github/:username", (req: Request, res: Response) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.githubClientId}&client_secret=${config.githubSecret}`,
+      method: "GET",
+      header: {"user-agent": "nodejs"},
+    }
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode != 200) {
+        return res.status(404).json({ msg: "Github profile not found" });
+      }
+
+      res.json(JSON.parse(body));
+    })
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 /**
  *  @route GET api/profile/me
