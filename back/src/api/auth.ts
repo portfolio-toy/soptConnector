5a164bc47080a4f792867b19a0a5dcd72e5a423f
Kim HyunJi <khyunjiee@gmail.com>
2021-05-07 15:34:06 +0900

    feat: 로그인 라우터 구현

--- back/src/api/auth.ts
+++ back/src/api/auth.ts
@@ -20,7 +20,50 @@ router.post(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Password is required").exists(),
   ],
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+      const error = validationResult(req);
+      if (!error.isEmpty()) {
+          res.status(400).json({ error: error.array() });
+      }
+
+      const { email, password } = req.body;
+
+      try {
+          let user = await User.findOne({ email });
+          if (!user) {
+              res.status(400).json({
+                  errors: [{ msg: "User not exists" }],
+              });
+          }
+
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) {
+              res.status(400).json({
+                  errors: [{ msg: "Password is not match" }],
+              });
+          }
+
+          await user.save();        // 로그인에서 user.save가 필요한 이유?
+
+          const payload = {
+              user: {
+                  id: user.id,
+              },
+          };
+          jwt.sign(
+              payload,
+              config.jwtSecret,
+              { expiresIn: 36000 },
+              (err, token) => {
+                  if (err) throw err;
+                  res.json({ token });
+              }
+          );
+      } catch (err) {
+          console.error(err.message);
+          res.status(500).send("Server Error");
+      }
+  }
 );
 
 /*
