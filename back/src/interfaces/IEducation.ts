e20131136717dce24b3cc0da4e60169955d40790
최영재 <brandon94_@naver.com>
2021-05-08 16:40:59 +0900

    practice

---
+++ back/src/interfaces/IEducation.ts
@@ -0,0 +1,19 @@
+export interface IEducation{
+    school: string;
+    degree: string;
+    fieldofstudt: string;
+    from: Date;
+    to: Date;
+    current: Boolean;
+    description: string;
+}
+
+// export interface IEducationInputDTO {
+//     school: string;
+//     degree: string;
+//     fieldofstudy: string;
+//     from: Date;
+//     to: Date;
+//     current: boolean;
+//     description: string;
+//   }
---
+++ back/src/interfaces/ISocial.ts
@@ -0,0 +1,15 @@
+export interface ISocial{
+    youtube: string;
+    twitter: string;
+    facebook: string;
+    linkedin: string;
+    instagram: string;
+}
+
+// export interface ISocialInputDTO {
+//     youtube: String;
+//     twitter: String;
+//     facebook: String;
+//     linkedin: String;
+//     instagram: String;
+//   }
