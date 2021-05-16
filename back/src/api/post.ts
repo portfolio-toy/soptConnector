e7d189250887d299a1a68a195e4ecd9e9b19dbaf
wngus4296 <wngus4296@swu.ac.kr>
2021-05-17 02:36:30 +0900

    수정중

--- back/src/api/post.ts
+++ back/src/api/post.ts
@@ -47,7 +47,6 @@ router.post(
  *  @desc Get all posts
  *  @access Private
  */
-// 전체 게시물 조회
 router.get("/", auth, async (req: Request, res: Response) => {
   try {
     const posts = await Post.find().sort({ date: -1 });
@@ -63,47 +62,46 @@ router.get("/", auth, async (req: Request, res: Response) => {
  *  @desc Get post by ID
  *  @access Private
  */
-//해당 게시글 조회
-router.get("/:id",
-auth,
-async(req: Request, res: Response) => {
+router.get("/:id", auth, async (req: Request, res: Response) => {
   try {
     const post = await Post.findById(req.params.id); // 여기 req.params.id에는 url경로에 있는 :id 값이 들어옴.
 
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
     res.json(post);
   } catch (error) {
     console.error(error.message);
-    if (error.kind == "ObjectId") {
-      return res.status(404).json({ msg: "Post not found"});
+    if (error.kind === "ObjectId") { // ==와 ===의 차이 ==는 동등연산자, ===는 일치 연산자 ===가 더 정확하게 비교함
+      return res.status(404).json({ msg: "Post not found" });
     }
     res.status(500).send("Server Error");
   }
 });
 
 /**
-*  @route DELETE api/posts/:id
-*  @desc Delete a post
-*  @access Private
-*/
-// 해당 게시물 삭제
-router.delete("/:id",
-auth,
-async(req: Request, res: Response) => {
+ *  @route DELETE api/posts/:id
+ *  @desc Delete a post
+ *  @access Private
+ */
+router.delete("/:id", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-
-    if(post.user.toString() !== req.body.user.id) {
-      return res.status(401).json({msg: "User not Authorized"});
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (post.user.toString() !== req.body.user.id) {
+      return res.status(401).json({ msg: "User not Authorized" });
     }
     await post.remove();
 
-    res.json({msg: "Post Removed"});
-  } catch(error) {
+    res.json({ msg: "Post Removed" });
+  } catch (error) {
     console.error(error.message);
-    if(error.kind === "ObjectId") { // ==와 ===의 차이 ==는 동등연산자, ===는 일치 연산자 ===가 더 정확하게 비교함
-      return res.status(404).json({msg: "Post not found"});
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
     }
-    res.status(500).send("Server Error"); 
+    res.status(500).send("Server Error");
   }
 });
 
