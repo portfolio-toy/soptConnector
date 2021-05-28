347bae6b8265ae5a0ae0719e232ed699688aaf92
최영재 <brandon94_@naver.com>
2021-05-29 00:44:36 +0900

    브랜치 생성

--- back/src/api/auth.ts
+++ back/src/api/auth.ts
@@ -20,49 +20,7 @@ router.post(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Password is required").exists(),
   ],
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const { email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if (!user) {
-        res.status(400).json({
-          errors: [{ msg: "Invalid Credentials" }],
-        });
-      }
-      // Encrpyt password
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        res.status(400).json({
-          errors: [{ msg: "Invalid Credentials" }],
-        });
-      }
-
-      // Return jsonwebtoken
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      jwt.sign(
-        payload,
-        config.jwtSecret,
-        { expiresIn: 36000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
+  async (req: Request, res: Response) => {}
 );
 
 /*
@@ -70,14 +28,6 @@ router.post(
  *  @desc Test Route
  *  @access Public
  */
-router.get("/", auth, async function (req: Request, res: Response) {
-  try {
-    const user = await User.findById(req.body.user.id).select("-password");
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Err");
-  }
-});
+router.get("/", auth, async function (req: Request, res: Response) {});
 
 module.exports = router;
