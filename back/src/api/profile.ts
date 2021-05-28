0b65f73cae5e2253ddc9c31d8c9ec35b4a7420de
최영재 <brandon94_@naver.com>
2021-05-29 02:19:48 +0900

    3rd homework ing

--- back/src/api/profile.ts
+++ back/src/api/profile.ts
@@ -14,14 +14,38 @@ const router = Router();
  *  @desc Get all profiles
  *  @access Public
  */
-router.get("/", async (req: Request, res: Response) => {});
+ router.get("/", async (req,res) => {
+  try{
+      const profiles = await Profile.find().populate("user",["name", "avatar"]);
+      res.json(profiles);
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+  }
+});
 
 /**
  *  @route GET api/profile/user/:user_id
  *  @desc Get profile by user ID
  *  @access Public
  */
-router.get("/user/:user_id", async (req: Request, res: Response) => {});
+router.get("/user/:user_id", async (req: Request, res: Response) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user",["user","avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+
+});
 
 /**
  *  @route GET api/profile/github/:username
