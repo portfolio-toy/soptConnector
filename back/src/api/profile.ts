8a2fbedbb76e473c650bd26ad0616735e093ea4d
sssua-0928 <hansua990928@gmail.com>
2021-05-29 14:51:52 +0900

    5

--- back/src/api/profile.ts
+++ back/src/api/profile.ts
@@ -177,5 +177,105 @@ router.post(
   }
 );
 
+// 경력 추가
+/**
+ *  @route PUT api/profile/experience
+ *  @desc Add profile experience
+ *  @access Private
+ */
+router.put(
+"/experience",
+auth,
+[
+  check("title", "Title is required").not().isEmpty(),
+  check("company", "Company is required").not().isEmpty(),
+  check("from", "From data is required").not().isEmpty(),
+],
+async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description,
+  } = req.body;
 
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description,
+  };
+
+  try {
+    const profile = await Profile.findOne({ user: req.body.user.id });
+    profile.experience.unshift(newExp);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+}
+);
+
+// 학력 추가
+/**
+ *  @route PUT api/profile/education
+ *  @desc Add profile education
+ *  @access Private
+ */
+router.put(
+"/education",
+auth,
+[
+  check("school", "School is required").not().isEmpty(),
+  check("degree", "Degree is required").not().isEmpty(),
+  check("fieldofstudy", "Field of study is required").not().isEmpty(),
+],
+async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description,
+  } = req.body;
+
+  const newEdu = {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description,
+  };
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.education.unshift(newEdu);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+}
+);
 module.exports = router;
